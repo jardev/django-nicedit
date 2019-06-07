b9e0435a4396cc3a708b6c4dadfada6d5c570b58
Saurabh <MetalBlazer>
2019-06-07 16:01:08 +0530

    Support django 1.11

--- nicedit/urls.py
+++ nicedit/urls.py
@@ -1,10 +1,9 @@
-from django.conf.urls import patterns, url
+from django.conf.urls import url
+
+from .views import upload
 
 
-__all__ = ('urlpatterns',)
 
-
-urlpatterns = patterns(
-    'nicedit.views',
-    url(r'^upload/$', 'upload', name='nicedit_upload'),
-)
+urlpatterns = [
+    url(r'^upload/$', upload, name='nicedit_upload'),
+]
